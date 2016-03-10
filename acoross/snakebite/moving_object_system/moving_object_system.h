33db64d7d546c7e1a4c5f42ea60dee92d6c86ad8
acoross <acoross@gmail.com>
2016-03-10 23:48:43 +0900

    프로젝트 구조 변경

--- acoross/snakebite/moving_object_system/moving_object_system.h
+++ acoross/snakebite/moving_object_system/moving_object_system.h
@@ -1,8 +1,8 @@
 #ifndef SNAKEBITE_MOVING_OBJECT_SYSTEM_MOVING_OBJECT_SYSTEM_H_
 #define SNAKEBITE_MOVING_OBJECT_SYSTEM_MOVING_OBJECT_SYSTEM_H_
 
-#include "geo_types.h"
-#include "moving_object_container.h"
-#include "moving_object.h"
+#include "details/geo_types.h"
+#include "details/moving_object_container.h"
+#include "details/moving_object.h"
 
 #endif //SNAKEBITE_MOVING_OBJECT_SYSTEM_MOVING_OBJECT_SYSTEM_H_
